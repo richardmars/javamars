b08a04f0d0814fa42d40a1ec8b9001166c7f0188
unknown <>
2017-05-12 08:28:00 +0800

    chain pattern

---
+++ src/main/java/io/github/richardmars/designpattern/chain/ChainPatternDemo.java
@@ -0,0 +1,20 @@
+package io.github.richardmars.designpattern.chain;
+
+public class ChainPatternDemo {
+	public static AbstractLogger getChainOfLoggers() {
+		AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+		AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+		AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+		
+		errorLogger.setNextLogger(fileLogger);
+		fileLogger.setNextLogger(consoleLogger);
+		
+		return errorLogger;
+	}
+	
+	public static void main(String[] args) {
+		AbstractLogger loggerChain = getChainOfLoggers();
+		loggerChain.logMessage(AbstractLogger.INFO, "This is an information");
+		
+	}
+}
---
+++ src/main/java/io/github/richardmars/designpattern/chain/FileLogger.java
@@ -0,0 +1,14 @@
+package io.github.richardmars.designpattern.chain;
+
+public class FileLogger extends AbstractLogger {
+
+	public FileLogger(int level) {
+		this.level = level;
+	}
+	
+	@Override
+	protected void write(String message) {
+		System.out.println("File Console::Logger: " + message);
+	}
+
+}
